89fd17d6cddb02d6e02ab9ea8b06d363acc5818e
Kur4m4 <Kur4m4@users.noreply.github.com>
2018-01-31 16:32:53 -0200

    Add files via upload

---
+++ XF.Hello/XF.Hello/ConfigData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XF.Hello
+{
+    class ConfigData
+    {
+        private bool swtEmail;
+        private string email;
+
+        public bool SwtEmail
+        {
+            get
+            {
+                return swtEmail;
+            }
+            set
+            {
+                swtEmail = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+    }
+}
---
+++ XF.Hello/XF.Hello/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XF.Hello
+{
+	public partial class MainPage : ContentPage
+	{
+        ConfigData configData;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            if (configData == null)
+                configData = new ConfigData();
+        }
+
+        private void EnviarEmail(object sender, EventArgs e)
+        {
+            if (configData.SwtEmail)
+                DisplayAlert("Mensagem", string.Format("E-mail enviado para {0}", configData.Email), "OK");
+            else
+                DisplayAlert("Mensagem", "E-mail não autorizado", "OK");
+        }
+
+        private void BtnConfig_Clicked(object sender, EventArgs args)
+        {
+            Navigation.PushModalAsync(new ConfigPage() { BindingContext = configData });
+        }
+    }
+}
